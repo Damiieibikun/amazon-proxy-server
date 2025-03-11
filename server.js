4908ba95b51cf03b3eb55fc74c06f3f67fd2ce8d
Damilola Ibikunle <damiieibikun@gmail.com>
2025-03-11 18:09:22 +0100

    made changes

--- server.js
+++ server.js
@@ -11,23 +11,35 @@ require('dotenv').config()
 app.use(express.json());
 
 // Proxy requests to the HTTP API
+// app.use(
+//   "/api",
+//   createProxyMiddleware({
+//     target: process.env.REACT_APP_BASEURL, // Target API
+//     changeOrigin: true,
+//     secure: false, // Allow HTTP
+//     pathRewrite: { "^/api": "/v2" }, // Rewrite "/api" to "/v2"
+//     onProxyReq: (proxyReq, req, res) => {
+//       if (req.body) {
+//         let bodyData = JSON.stringify(req.body);
+//         proxyReq.setHeader("Content-Type", "application/json");
+//         proxyReq.setHeader("Content-Length", Buffer.byteLength(bodyData));
+//         proxyReq.write(bodyData);
+//       }
+//     },
+//   })
+// );
+
 app.use(
-  "/api",
-  createProxyMiddleware({
-    target: process.env.REACT_APP_BASEURL, // Target API
-    changeOrigin: true,
-    secure: false, // Allow HTTP
-    pathRewrite: { "^/api": "/v2" }, // Rewrite "/api" to "/v2"
-    onProxyReq: (proxyReq, req, res) => {
-      if (req.body) {
-        let bodyData = JSON.stringify(req.body);
-        proxyReq.setHeader("Content-Type", "application/json");
-        proxyReq.setHeader("Content-Length", Buffer.byteLength(bodyData));
-        proxyReq.write(bodyData);
-      }
-    },
-  })
-);
+    "/api",
+    createProxyMiddleware({
+      target: process.env.REACT_APP_BASEURL, 
+      changeOrigin: true,
+      secure: false,
+      pathRewrite: { "^/api": "/v2" },
+      timeout: 60000, // 60 seconds timeout
+      proxyTimeout: 60000,
+    })
+  );
 
 const PORT = process.env.REACT_APP_PORT;
 app.listen(PORT, () => {
