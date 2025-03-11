80c2c8a7f8ea901bb61000169eb1675593181326
Damilola Ibikunle <damiieibikun@gmail.com>
2025-03-11 14:27:17 +0100

    added server files

---
+++ server.js
@@ -0,0 +1,33 @@
+// server.js
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
+require('dotenv').config()
+// Proxy middleware
+app.use('/api', async (req, res) => {
+  try {
+    const targetURL = process.env.BASEURL + req.url.replace('/api', '');
+    const response = await axios({
+      method: req.method,
+      url: targetURL,
+      data: req.body,
+      headers: {
+        ...req.headers,
+        host: process.env.HOST // Override host if needed
+      }
+    });
+    res.send(response.data);
+  } catch (error) {
+    res.status(error.response?.status || 500).send(error.message);
+  }
+});
+
+app.options('*', cors());
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Proxy server running on port ${PORT}`));
+
